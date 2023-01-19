ba00118225f6e9edbf06d6c8090b9ff53337192b
t08mac23 <t08mac23@gmail.com>
2023-01-19 20:10:46 +0900

    ログイン結合テスト
    ユーザーのログインの結合テスト
    正常系
    異常系

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe 'ユーザー新規登録', type: :system do
   end
   context 'ユーザー新規登録ができるとき' do 
     it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # basic認証
       basic_pass root_path
       # トップページに移動する
       visit root_path
@@ -30,7 +31,7 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       }.to change { User.count }.by(1)
       # トップページへ遷移したことを確認する
       expect(current_path).to eq(root_path)
-      # カーソルを合わせるとログアウトボタンが表示されることを確認する
+      # ログアウトするボタンがあることを確認する
       expect(page
       ).to have_content('ログアウト')
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
@@ -57,7 +58,54 @@ RSpec.describe 'ユーザー新規登録', type: :system do
         find('input[name="commit"]').click
       }.to change { User.count }.by(0)
       # 新規登録ページへ戻されることを確認する
-      expect(current_path).to eq user_registration_path
+      expect(current_path).to eq(user_registration_path)
+    end
+  end
+end
+
+RSpec.describe 'ログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインができるとき' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      # basic認証
+      basic_pass root_path
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # 正しいユーザー情報を入力する
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # トップページへ遷移することを確認する
+      expect(current_path).to eq(root_path)
+      # ログアウトするボタンがあることを確認する
+      expect(page).to have_content('ログアウト')
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ログインができないとき' do
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # ユーザー情報を入力する
+      fill_in 'email', with: ''
+      fill_in 'password', with: ''
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq(user_session_path)
     end
   end
 end
