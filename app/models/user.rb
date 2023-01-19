d1414c2f3e307792ea93214f79b41049f8dbf5d8
t08mac23 <t08mac23@gmail.com>
2023-01-19 18:47:25 +0900

    userモデル単体テスト
    r-spec
    factory_bot
    faker
    の導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,    presence: :true
+         validates :nickname,    presence: :true, length: { maximum: 30 }
+         validates :password,    format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze }
 end
