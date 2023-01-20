d8036e390e6739991b5046a5931a8f6ab0647052
t08mac23 <t08mac23@gmail.com>
2023-01-20 14:51:31 +0900

    アクティブハッシュ：sweet_dry

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -5,6 +5,8 @@ class Drink < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+  belongs_to :sweet_dry
 
-  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :genre_id, numericality:     { other_than: 1, message: "can't be blank" }
+  validates :sweet_dry_id, numericality: { other_than: 1, message: "can't be blank" }
 end
