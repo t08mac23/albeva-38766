2ba69ab7066ea5e846956ea7999523676a5714bd
t08mac23 <t08mac23@gmail.com>
2023-01-20 14:11:07 +0900

    アクティブハッシュ：genre

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -2,4 +2,9 @@ class Drink < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
 end
