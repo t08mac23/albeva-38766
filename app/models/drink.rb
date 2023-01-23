f39d08ab4bd6d3fc74621bbf046df20a9b7bc244
t08mac23 <t08mac23@gmail.com>
2023-01-23 16:50:12 +0900

    アクティブハッシュ：recommendation
    ★の数で表した
    デプロイ後機能するか
    
    バリデーションの追記

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -6,7 +6,14 @@ class Drink < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
   belongs_to :sweet_dry
+  belongs_to :recommendation_id
 
-  validates :genre_id, numericality:     { other_than: 1, message: "can't be blank" }
-  validates :sweet_dry_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  with_options presence: true do
+    validates :image, :name, :description
+  end
+
+  validates :genre_id,          numericality: { other_than: 1, message: "can't be blank" }
+  validates :sweet_dry_id,      numericality: { other_than: 1, message: "can't be blank" }
+  validates :recommendation_id, numericality: { other_than: 1, message: "can't be blank" }
 end
