54cfc54f1dfbd7d88451c9f9702271bd73cd323d
t08mac23 <t08mac23@gmail.com>
2023-01-24 14:30:42 +0900

    モデル単体テスト

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -11,8 +11,11 @@ class Drink < ApplicationRecord
 
 
   with_options presence: true do
-    validates :image, :name, :alc_percent, :description
+    validates :image, :name, :description
   end
+
+  validates  :alc_percent, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 },
+                      presence: { message: "を入力してください" }
 
   validates :genre_id,          numericality: { other_than: 1, message: "can't be blank" }
   validates :sweet_dry_id,      numericality: { other_than: 1, message: "can't be blank" }
--- spec/factories/drinks.rb
+++ spec/factories/drinks.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :drink do
+    name                {Faker::Lorem.sentence}
+    description         {Faker::Lorem.sentence}
+    alc_percent         {Faker::Number.between(from: 0, to: 100)}
+    combo               {Faker::Lorem.sentence}
+    genre_id            {Faker::Number.between(from: 2, to: 11)}
+    sweet_dry_id        {Faker::Number.between(from: 2, to: 6)}
+    recommendation_id   {Faker::Number.between(from: 2, to: 6)}
+    best_time_id        {Faker::Number.between(from: 2, to: 11)}
     
+    association :user
+
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/f569f9fd1992c36a6c42c915abbd924c_w.jpeg'))}
   end
 end
