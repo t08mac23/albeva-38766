20291b307d3d56344901a270aabadfb85b38f48f
t08mac23 <t08mac23@gmail.com>
2023-02-09 18:07:11 +0900

    見た目の調整

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,15 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, drink_id: params[:drink_id])
