9cd6bf88ad296a0471dd2547ab740a4093515434
t08mac23 <t08mac23@gmail.com>
2023-02-11 16:21:10 +0900

    js.erbの作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,21 @@
 class CommentsController < ApplicationController
+
+  before_action :set_comment, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
+  # before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def create
-    @comment = Comment.create(comment_params)
+    @drink = Drink.find(params[:drink_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.drink_id = @drink.id
+    # @comment = Comment.create(comment_params)
     if @comment.save
-      redirect_to "/drinks/#{@comment.drink.id}"
+      flash.now[:notice] = 'コメントを投稿しました'
+    #   # redirect_to "/drinks/#{@comment.drink.id}"
+    #   render :drink_comments
     else
-      @drink = @comment.drink
-      @comments = @drink.comments
+    #   @drink = @comment.drink
+      # @comments = @drink.comments
       render "drinks/show"
     end
   end
@@ -14,13 +24,28 @@ class CommentsController < ApplicationController
   end
 
   def update
+    @comment.update
   end
 
   def destroy
+    @drink = Drink.find(params[:drink_id])  
+    @comment.destroy
+    flash.now[:alert] = '投稿を削除しました'
+    #renderしたときに@drinkのデータがないので@drinkを定義
+    # @drink = Drink.find(params[:drink_id])  
+    render :drink_comments  #render先にjsファイルを指定
   end
 
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, drink_id: params[:drink_id])
   end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  # def contributor_confirmation
+  #   redirect_to root_path unless current_user == @comment.user
+  # end
 end
