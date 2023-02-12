3f6e074e6ed44c06f91615f393a5604b5e83da3c
t08mac23 <t08mac23@gmail.com>
2023-02-13 03:08:06 +0900

    いいねを非同期通信
    yarnインストール
    environmentにJquery追加
    javascriptの改正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,17 +2,17 @@ class FavoritesController < ApplicationController
 
   before_action :authenticate_user!, only: [:create, :destroy]
   def create
+    @drink = Drink.find(params[:drink_id])
     drink = Drink.find(params[:drink_id])
     favorite = Favorite.new(drink_id: drink.id)
     favorite.user_id = current_user.id
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
+    @drink = Drink.find(params[:drink_id])
     drink = Drink.find(params[:drink_id])
     favorite = current_user.favorites.find_by(drink_id: drink.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
